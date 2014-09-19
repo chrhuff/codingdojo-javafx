aa524f0b33bfec1f8c0a20893ef2b1a238e3eefa
tahmed <tariq.ahmed@cofinpro.de>
2014-09-19 15:47:01 +0200

    voterservicce skelett

--- evote-server-api/src/main/java/de/cofinpro/codingdojo/server/api/Election.java
+++ evote-server-api/src/main/java/de/cofinpro/codingdojo/server/api/Election.java
@@ -1,6 +1,34 @@
 package de.cofinpro.codingdojo.server.api;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by tahmed on 19.09.2014.
  */
+@XmlRootElement(name = "election")
 public class Election {
+    private Integer id;
+    private String name;
+
+    public Election(Integer id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- evote-server-api/src/main/java/de/cofinpro/codingdojo/server/api/Voter.java
+++ evote-server-api/src/main/java/de/cofinpro/codingdojo/server/api/Voter.java
@@ -1,7 +1,10 @@
 package de.cofinpro.codingdojo.server.api;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by tahmed on 19.09.2014.
  */
+@XmlRootElement(name = "voter")
 public class Voter {
 }
