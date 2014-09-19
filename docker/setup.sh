aeb9d71d1292d29f8b2894b81c6ce39f15f620ba
Christian Huff <christian.huff@cofinpro.de>
2014-09-19 18:25:35 +0200

    Added Dockerfile, context.xml and tomcat-users.xml for building a modified tomcat8-java8 image.

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker run -d -p 3306:3306 --name=mariadb dockerfile/mariadb
-docker run -d -p 8080:8080 --name=tomcat --link mariadb:mariadb komu/tomcat8-java8
+docker run -d -p 8080:8080 --name=tomcat --link mariadb:mariadb codingdojo/tomcat8
