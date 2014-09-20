afc41792b3cfd49999740982bf427d8e5bb5e598
tahmed <tariq.ahmed@cofinpro.de>
2014-09-20 17:34:51 +0200

    Backend GUI

--- evote-client/src/main/java/de/cofinpro/codingdojo/client/BackendController.java
+++ evote-client/src/main/java/de/cofinpro/codingdojo/client/BackendController.java
@@ -5,10 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 
 import javax.inject.Inject;
@@ -16,57 +13,12 @@ import java.util.List;
 
 public class BackendController {
 
-    @FXML
-    private ListView party;
-    @Inject
-    private VoterService voterService;
-    @Inject
-    private ElectionService electionService;
-
-    @FXML
-    public void vote() {
-    }
-
-    @FXML
-    private ComboBox<Election> election;
-
-    @FXML
-    private Label label;
-    @FXML
-    private TextField nameField;
-
-    @FXML
-    private GridPane pane1;
-    @FXML
-    private GridPane pane2;
-    @Inject
-    private FXMLLoader fxmlLoader;
-
-    private Election selectedElection;
-
-    @FXML
-    public void registerVoter() {
-
-        Voter voter = new Voter();
-        voter.setName(nameField.getText());
-        Long myId = voterService.register(voter);
-
-        List<Election> elections = electionService.getElections();
-        election.setItems(FXCollections.observableList(elections));
-
-        election.getSelectionModel().selectedItemProperty().addListener((selected, oldElection, newElection) -> {
-            if (newElection != null) {
-                ObservableList<Party> list = FXCollections.observableArrayList(
-                        electionService.getParties(newElection.getId()));
-                party.setItems(list);
-                selectedElection = newElection;
-            }
-        });
-        pane1.setVisible(false);
-        pane2.setVisible(true);
-    }
-
-    @FXML
-    public void castVote() {
-    }
+    public ComboBox electionComboBox;
+    public ComboBox approvalComboBox;
+    public Button approveBtn;
+    public Button rejectBtn;
+    public ComboBox electionForVoteComboBox;
+    public ComboBox partyForVoteComboBox;
+    public TextField voteField;
+    public Button voteBtn;
 }
