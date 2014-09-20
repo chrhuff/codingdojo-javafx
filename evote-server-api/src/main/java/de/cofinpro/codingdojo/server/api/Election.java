0f67774bb178b5816cad608016bb65304d06662a
Christian Huff <christian.huff@cofinpro.de>
2014-09-20 13:50:04 +0200

    Added toString methods

--- evote-server-api/src/main/java/de/cofinpro/codingdojo/server/api/Election.java
+++ evote-server-api/src/main/java/de/cofinpro/codingdojo/server/api/Election.java
@@ -40,4 +40,9 @@ public class Election {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
 }
